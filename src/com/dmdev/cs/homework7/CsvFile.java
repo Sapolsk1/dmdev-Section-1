bb2759fc9dba2d0c7092b44fa476d788254fc358
Nursultan Unbaev <unbaevn@gmail.com>
2023-12-12 16:31:52 +0300

    additional lectures

---
+++ src/com/dmdev/cs/homework7/CsvFile.java
@@ -0,0 +1,30 @@
+package com.dmdev.cs.homework7;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CsvFile {
+
+    private Map<String, String> keyVal;
+
+    public CsvFile(String id) {
+        keyVal = new LinkedHashMap<>();
+    }
+
+    public Map<String, String> getKeyVal() {
+        return keyVal;
+    }
+
+    public void setKeyVal(Map<String, String> keyVal) {
+        this.keyVal = keyVal;
+    }
+
+    public void put(String key, String val) {
+        keyVal.put(key, val);
+    }
+
+    public String get(String key) {
+        return keyVal.get(key);
+    }
+
+}
---
+++ src/com/dmdev/cs/homework7/Main.java
@@ -0,0 +1,8 @@
+package com.dmdev.cs.homework7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
